abb534253a638e5456935e64971d8b6c340f3681
taojonesin <taojonesin (at) nowhere.com>
2014-06-24 01:14:22 -0700

    actually getting results from form data processing

--- server/request-handler.js
+++ server/request-handler.js
@@ -4,6 +4,11 @@
  * You'll have to figure out a way to export this function from
  * this file and include it in basic-server.js so that it actually works.
  * *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html. */
+var http = require('http');
+var url = require('url');
+var querystring = require('querystring');
+
+var content = {results: []};
 
 exports.handleRequest = function(request, response) {
   /* the 'request' argument comes from nodes http module. It includes info about the
@@ -11,14 +16,35 @@ exports.handleRequest = function(request, response) {
 
   /* Documentation for both request and response can be found at
    * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html */
+  //var path = url.parse(request.url).pathname;
+  var data = '';
+  var postData = '';
+  request.addListener('data', function(chunk) {
+    postData += chunk;
+  });
+  request.addListener('end', function(chunk) {
+    console.log("postData");
+    console.log(postData);
+  });
+  // request.on('data', function(chunk) {
+  //   console.log('Reading chunk: ' + chunk);
+  //   data += chunk;
+  // });
+  // request.on('end', function() {
+  //   postData = querystring.parse(data);
+  // });
 
   console.log("Serving request type " + request.method + " for url " + request.url);
-
-  if (request.method === 'POST' || request.url === '/send') {
-    // console.log("Yo, we just asked for send");
+  // TODO: Move this into routes or something like that
+  if (request.method === 'POST' && request.url === '/classes/messages') {
+    // console.log("POSTDATA");
+    // console.log(postData);
+    content.results.push(postData);
     var statusCode = 201;
-  } else {
+  } else if (request.method === 'GET' && request.url === '/classes/messages') {
     var statusCode = 200;
+  } else {
+    var statusCode = 404;
   }
 
   /* Without this line, this server wouldn't work. See the note
@@ -34,8 +60,7 @@ exports.handleRequest = function(request, response) {
    * anything back to the client until you do. The string you pass to
    * response.end() will be the body of the response - i.e. what shows
    * up in the browser.*/
-  var content = JSON.stringify({results: ["Hello, World"]});
-  response.end(content);
+  response.end(JSON.stringify(content));
 };
 
 /* These headers will allow Cross-Origin Resource Sharing (CORS).
